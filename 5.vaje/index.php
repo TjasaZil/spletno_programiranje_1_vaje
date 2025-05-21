f6b6de59ad1c420174cbeb5b7134b9ad56538158
TjasaZil <tjasa.zilavec@gmail.com>
2025-05-21 16:15:19 +0200

    5.vaje

---
+++ 5.vaje/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "Danes je ponedeljek<br/><br/>"; // to je izpis nekega texta na strani
+// print("Jutri bo torek");
+
+// spremenljivke
+$myName = "Janez";
+$myName = "Janko"; // String
+$dayInWeek = 1; // numeric
+$isNight = true; // boolean (true ali false)
+
+echo "<p class='main-text'>Pozdravljen, $myName, danes je $dayInWeek dan v tednu, trenutno je noč: $isNight</p>";
+?>
